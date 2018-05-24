64845108946470917a79d6c89c746a35f5ef54c0
Kevin Xue <hsb2185@autuni.ac.nz>
2018-05-25 11:33:49 +1200

    add file

---
+++ builtins.js
@@ -0,0 +1,124 @@
+module.exports = {
+  BuiltinBaseJoke: [
+    'Tell me a joke!',
+    'We could use some humor here...',
+    'How about a joke?',
+    'I\'d do well with a laugh right about now.',
+    'Joke around with use please :)'
+  ],
+  SimonSays: [
+    'Repeat back to me lorem ipsum dolor amet',
+    'echo back to me lorem ipsum dolor amet',
+    'simon says lorem ipsum dolor amet',
+    'You should repeat this back to me lorem ipsum dolor amet',
+    'lorem ipsum dolor amet is what you should say back to me',
+    'be a good bot and parrot benf fodjsi bdfsjdhf bazz back to me',
+    'copy cat behf sdkjhfg peiurf fdlkgj kwjhe foufg'
+  ],
+    "CatFacts": [
+    "give me a cat fact",
+    "tell me a cat fact",
+    "tell me something about cats",
+    "cat fact meow",
+    "meow meow"
+  ],
+    Uptime: [
+    'uptime',
+    'are you alive?',
+    'are you up?',
+    'status',
+    'current status',
+    'who are you?',
+    'what are you?',
+    'are you alive',
+    'are you up',
+    'who are you',
+    'what are you'    
+  ],
+    "ctis": [
+    "What is ctis?"
+    
+    ],
+     "COMP500Programming1": [
+    "What is programming1?",
+    "what is comp500?",
+    "what is comp500 programming?"
+  ],
+  "COMP503Programming2": [
+    "What is programming1?",
+    "What is programming2?",
+    "What is comp503?",
+    "what is comp503 programming?"
+  ],
+  "COMP603ProgramDesign&amp;Construction": [
+    "What is pdc?",
+    "what is program design&amp;construction?",
+    "what is comp603?",
+    "what is COMP603?"
+  ],
+  "COMP602SoftwareDevelopmentPractice": [
+    "what is SDP?",
+    "what is the pre-requisite and co-requisite for paper SDP?"
+  ],
+  "Software Development": [
+    "How many point in software development?",
+    "How long does this major studying?"
+  ],
+  "BCIS Structure": [
+    "what specialities are there in BCIS?",
+    "what BCIS major inclued?",
+    "what's BCIS structure?"
+  ],
+  "SoftwareDevelopmentS1": [
+    "what's the paper of software development for the first year paper in BCIS?",
+    "what's the paper of sd for the first year paper in BCIS?",
+    "what's the core paper of BCIS in the first year?"
+  ],
+  "S1MP": [
+    "S1MP",
+    "s1mp"
+  ],
+  "BCIS.pdf": [
+    "please send me BCIS structure."
+  ],
+  "SoftwareDevelopmentS2": [
+    "what's the paper of software development for the second year paper in BCIS?",
+    "what's the paper of sd for the second year paper in BCIS?",
+    "what's the core paper of BCIS in the second year?"
+  ],
+  "S2SD": [
+    "s2sd",
+    "S2SD"
+  ],
+  "SoftwareDevelopmentS3": [
+    "what's the paper of software development for the third year paper in BCIS?",
+    "\"what's the paper of sd for the third year paper in BCIS?\",",
+    "\"what's the core paper of BCIS in the third year?\""
+  ],
+  "S3ET": [
+    "s3et",
+    "S3ET",
+    "What is graduate condition in BCIS?"
+  ],
+  "Pre-req paper": [
+    "which paper have pre request in first year?"
+    
+  ],
+  "pre-req y2": [
+    "which paper have pre request in second year?"
+  ],
+  "Pre-req paper y3": [
+    "which paper have pre request in third year?"
+  ],
+  "welcome": [
+    "Hello",
+    "hi",
+    "hi, there",
+    "hello"
+  ],
+  "INFS600 Data &amp; Process Modelling": [
+    "what is INF600?",
+    "what is DPM?",
+    "what is Data &amp; Process Modelling?"
+  ]
+};
