55bc0734fbb4f8b26fb11122e58f02856e4315c8
kevin5031 <hsb2185@autuni.ac.nz>
2018-05-27 19:02:21 +1200

    Add files via upload

--- builtins.js
+++ builtins.js
@@ -40,13 +40,15 @@ module.exports = {
     
     ],
      "COMP500Programming1": [
+    "What is programming 1?",
     "What is programming1?",
     "what is comp500?",
+    "programming 1",
     "what is comp500 programming?"
   ],
   "COMP503Programming2": [
-    "What is programming1?",
     "What is programming2?",
+    "What is programming 2?",
     "What is comp503?",
     "what is comp503 programming?"
   ],
@@ -120,5 +122,116 @@ module.exports = {
     "what is INF600?",
     "what is DPM?",
     "what is Data &amp; Process Modelling?"
+  ],
+  "programming": [
+    "programming",
+    "Programming"
+  ],
+  "COMM501 Applied Communication": [
+    "What is Applied Communication?",
+    "what is Applied Communication?",
+    "what is comm501?",
+    "What is COMM501?"
+  ],
+  "COMP502 Foundations of IT Infrastructure": [
+    "what is FOIT?",
+    "what is COMP502 Foundations of IT Infrastructure?",
+    "what is Foundations of IT Infrastructure?",
+    "what is comp502?"
+  ],
+  "INFS500 Enterprise Systems": [
+    "what is INFS500?",
+    "what is enterprise systems?",
+    "what is infs500?"
+  ],
+  "ENEL504 Computer Network Principles (CCNA1)": [
+    "what is ccna1?",
+    "what is ENEL504 Computer Network Principles?",
+    "what is ENEL504?"
+  ],
+  "INFS601 Logical Database Design": [
+    "what is ldd?",
+    "what is infs601?",
+    "what is Logical Database Design?"
+  ],
+  "COMP604 Operating Systems": [
+    "what is COMP604 Operating Systems?",
+    "what is COMP604?",
+    "what is Operating Systems code?"
+  ],
+  "INFS602 Physical Database Design": [
+    "what is INFS602 Physical Database Design?",
+    "what is INFS602?",
+    "what is Physical Database Design?"
+  ],
+  "ENSE701 Software Engineering": [
+    "what is ENSE701 Software Engineering?",
+    "what is ENSE701?",
+    "what is Software Engineering?"
+  ],
+  "COMP719 Applied Human Computer Interaction": [
+    "what is COMP719 Applied Human Computer Interaction?",
+    "what is COMP719?",
+    "what is Applied Human Computer Interaction?",
+    "what is ahci?"
+  ],
+  "COMP721 Web Development": [
+    "what is COMP721 Web Development?",
+    "what is COMP721?",
+    "what is Web Development?",
+    "what is wd?"
+  ],
+  "COMP713 Distributed &amp; Mobile Systems": [
+    "what is COMP713 Distributed &amp; Mobile Systems?",
+    "what is COMP713?",
+    "what is Distributed &amp; Mobile Systems?",
+    "what is dms?"
+  ],
+  "R&amp;D": [
+    "what is R&amp;D code?",
+    "what is research paper?",
+    "what is Research &amp; Development Project?",
+    "what is Research &amp; Development Project include?"
+  ],
+  "condition of p1": [
+    "Which courses require p1 as a pre request?",
+    "course with p1 pre request"
+  ],
+  "fail paper y1": [
+    "what are the paper restriction if I failed one or more of aut bcis paper?",
+    "If I failed paper what I need to do next step?"
+  ],
+  "condition of pdc": [
+    "which courses require pdc as a pre request?",
+    "course with pdc pre request"
+  ],
+  "condition of itpm": [
+    "which courses require itpm as a pre request?",
+    "course with itpm pre request"
+  ],
+  "paper without pre request": [
+    "pre request of ac",
+    "pre request of COMM501 Applied Communication",
+    "pre request of ctis",
+    "pre request of COMP501 Computing Technology in Society",
+    "pre request of foit",
+    "pre request of COMP502 Foundations of IT Infrastructure",
+    "pre request of es",
+    "pre request of INFS500 Enterprise Systems",
+    "pre request of ccna1",
+    "pre request of ENEL504 Computer Network Principles",
+    "pre request of itpm",
+    "pre request of COMP600 IT Project Management",
+    "pre request of ahci",
+    "pre request of COMP719 Applied Human Computer Interaction"
+  ],
+  "question format": [
+    "question format",
+    "format"
+  ],
+  "related job": [
+    "what can I do after graduate?",
+    "after software development graduate?",
+    "what papers from AUT BCIS are suitable for a specific IT related job?"
   ]
 };
